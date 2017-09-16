6b271e78c5f937f1e4000fc5ccefb652d9695368
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-09-16 17:57:25 +0100

    added root level templates folder, base.html and blogposts.html

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('blog_app.urls'))
+    url(r'', include('blog_app.urls')) # include the urls in the blog_app
 ]
